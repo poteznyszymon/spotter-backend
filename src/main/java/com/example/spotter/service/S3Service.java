d59f097ae2ed4bf8b68febc60838e6010199bebe
szymon <szymonfularczyk@o2.pl>
2026-02-17 15:47:38 +0100

    added shell component to list buckets, objects and deleting objects in s3 via shell, improve error handling in s3 service

--- src/main/java/com/example/spotter/service/S3Service.java
+++ src/main/java/com/example/spotter/service/S3Service.java
@@ -3,13 +3,16 @@ package com.example.spotter.service;
 import com.example.spotter.exception.exceptions.S3FileUploadException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -25,7 +28,9 @@ public class S3Service {
     }
 
     public String uploadFile(MultipartFile file, String bucketName) {
-        String fileName = UUID.randomUUID().toString();
+        String randomUuid = UUID.randomUUID().toString();
+        String extension = StringUtils.getFilenameExtension(file.getOriginalFilename());
+        String fileName = extension != null ? randomUuid + "." + extension : randomUuid;
         try {
             PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                     .bucket(bucketName)
@@ -35,6 +40,10 @@ public class S3Service {
 
             s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
             return fileName;
+        } catch (S3Exception e) {
+            throw new RuntimeException("Could not upload file from S3 (AWS Error)", e);
+        } catch (SdkClientException e) {
+            throw new RuntimeException("Failed to upload file from S3 (Connection Error)", e);
         } catch (IOException e) {
             throw new S3FileUploadException("Error while uploading file to S3");
         }
@@ -48,14 +57,50 @@ public class S3Service {
                     .build();
 
             s3Client.deleteObject(deleteObjectRequest);
-        } catch (Exception e) {
-            /// TODO add logs here
+        } catch (S3Exception e) {
+            throw new RuntimeException("Could not delete file from S3 (AWS Error)", e);
+        } catch (SdkClientException e) {
+            throw new RuntimeException("Failed to delete file from S3 (Connection Error)", e);
+        }
+    }
+
+    public List<String> listBuckets() {
+        try {
+            return s3Client.listBuckets().buckets()
+                    .stream()
+                    .map(Bucket::name)
+                    .toList();
+        } catch (S3Exception e) {
+            throw new RuntimeException("Failed to list S3 buckets (AWS Error)", e);
+        } catch (SdkClientException e) {
+            throw new RuntimeException("Failed to list S3 buckets (Connection Error)", e);
+        }
+    }
+
+    public List<String> listObject(String bucketName) {
+        try {
+            ListObjectsV2Request listObjectsV2Request = ListObjectsV2Request.builder()
+                    .bucket(bucketName)
+                    .build();
+
+            ListObjectsV2Response response = s3Client.listObjectsV2(listObjectsV2Request);
+            return response.contents()
+                    .stream()
+                    .map(S3Object::key)
+                    .toList();
+        } catch (S3Exception e) {
+            throw new RuntimeException("Could not list all buckets from S3 (AWS Error)", e);
+        } catch (SdkClientException e) {
+            throw new RuntimeException("Failed to list all buckets from from S3 (Connection Error)", e);
         }
     }
 
     public String getPublicUrl(String bucketName) {
-        if (bucketName == null || !publicUrl.contains("{bucket}")) {
-            throw new RuntimeException("Error while getting bucket public url");
+        if (bucketName == null || bucketName.isBlank()) {
+            throw new IllegalArgumentException("Bucket name cannot be null or empty");
+        }
+        if (!publicUrl.contains("{bucket}")) {
+            throw new IllegalStateException("Application configuration error: Invalid S3 Public URL format");
         }
         return publicUrl.replace("{bucket}", bucketName);
     }
